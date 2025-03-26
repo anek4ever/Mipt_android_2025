b8a875ebd4edb7b7303d8d1320189e9764f8a420
anek4ever <124886669+anek4ever@users.noreply.github.com>
2025-03-26 23:40:45 +0300

    Delete Main.kt

--- Main.kt
+++
@@ -1,9 +0,0 @@
-package org.example
-
-fun Array<Int>.sumCubes(): Int = this.filter { it % 2 != 0 }
-    .sumOf { it * it * it }
-
-fun main() {
-    val numbers = arrayOf(1, 2, 3, 4)
-    println(numbers.sumCubes())
-}
